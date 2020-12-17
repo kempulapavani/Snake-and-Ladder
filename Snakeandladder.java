726adefb7b7615d0c59e232118d90a18d8cd6fdc
kempulapavani <72300601+kempulapavani@users.noreply.github.com>
2020-12-17 10:31:36 +0530

    added Initial position

---
+++ Snakeandladder.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadder {
+         public int initialPosition=0;
+         public static void main(String[] arg) {
+                 System.out.println("Welcome to Snake and Ladder");
+         }
+}
+
